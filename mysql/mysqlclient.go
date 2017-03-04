6d46a01b074d24b8cf1e675b45af1dc1e166dad4
maxwell92 <710028463@qq.com>
2017-03-04 21:40:54 +0800

    replace the old config

--- mysql/mysqlclient.go
+++ mysql/mysqlclient.go
@@ -1,10 +1,9 @@
 package mysql
 
 import (
-	mylog "app/backend/common/util/log"
-	config "app/backend/common/yce/config"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	mylog "github.com/maxwell92/newutils/log"
 	"sync"
 	"time"
 )
@@ -13,20 +12,12 @@ var log = mylog.Log
 
 type MysqlClient struct {
 	DB *sql.DB
-	/*
-		host     string
-		user     string
-		password string
-		database string
-		pool     int
-	*/
 }
 
 var instance *MysqlClient
 
 var once sync.Once
 
-// func NewMysqlClient(host, user, password, database string, pool int) *MysqlClient {
 func MysqlInstance() *MysqlClient {
 	once.Do(func() {
 		instance = new(MysqlClient)
@@ -34,12 +25,16 @@ func MysqlInstance() *MysqlClient {
 	return instance
 }
 
-func (c *MysqlClient) Open() {
-	// endpoint := DB_USER + ":" + DB_PASSWORD + "@tcp(" + DB_HOST + ")/" + DB_NAME + DB_CONNECTION_SUFFIX
+type MySQLConfig struct {
+	Driver    string
+	Endpoints string
+	MaxOpen   int
+	MaxIdle   int
+}
 
-	endpoint := config.Instance().GetDbEndpoint()
+func (c *MysqlClient) Open(c *MySQLConfig) {
 
-	db, err := sql.Open(config.DATABASE_DRIVER, endpoint)
+	db, err := sql.Open(c.Driver, c.Endpoints)
 
 	if err != nil {
 		log.Fatalf("MysqlClient Open Error: err=%s", err)
@@ -47,8 +42,8 @@ func (c *MysqlClient) Open() {
 	}
 
 	// Set Connection Pool
-	db.SetMaxOpenConns(config.Instance().RedisMaxActiveConn)
-	db.SetMaxIdleConns(config.Instance().RedisMaxIdleConn)
+	db.SetMaxOpenConns(c.MaxOpen)
+	db.SetMaxIdleConns(c.MaxIdle)
 
 	c.DB = db
 
